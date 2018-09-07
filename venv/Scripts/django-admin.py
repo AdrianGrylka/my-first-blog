f67c26b30b6cdc05b0031e3663885823b08a7a7f
Adrian Grylka <grylka_a@psi.ch>
2018-09-07 10:54:41 +0200

    My Djangoi Girls app, first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Adrian Grylka\PycharmProjects\DjangoGirlsTutorial2\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
